d442dbfb46751a21040b5fb79ee326b02e031ab9
Michael Vicente <mvicente@Michaels-Air.lan>
2025-11-27 17:09:42 +0800

    Fix React Router basename for custom domain and add fallback API URL

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/pottery-booking-flow">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
